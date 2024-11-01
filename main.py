42e1b241be6f507bf4a930fbab051a65a3ed878b
RafiHul <rafihul2905@gmail.com>
2024-11-01 08:38:24 +0700

    initial commit

--- main.py
+++ main.py
@@ -5,16 +5,18 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
+import json
 import time
 import requests
 
-chrome_driver_path = r"your path"
+limit = int(input("Data Limit : "))
+chrome_driver_path = r"C:\Users\rafih\Desktop\chromedriver-win64\chromedriver.exe"
 main_url = "https://store.steampowered.com/search/?sort_by=_ASC&hidef2p=1&filter=topsellers&supportedlang=english"
+json_name = "data.json"
 
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
 driver.get(main_url)
-
 
 try:
     WebDriverWait(driver, 10).until(
@@ -34,10 +36,12 @@ driver.quit()
 find_links = soup.find_all("a", {"class": "search_result_row ds_collapse_flag"})
 find_final_price = soup.find_all("div",{"class":"discount_final_price"})
 
+n = 0
 for i,j in zip(find_links, find_final_price):
-    response = requests.get(i.get("href"), headers={"UserAgents": UserAgent().chrome})
+    url: str = i.get("href")
+    response = requests.get(url, headers={"UserAgents": UserAgent().chrome})
     soup = BeautifulSoup(response.text, "html.parser")
-
+    get_title_from_url = url.split("/")[5]
     try:
         # get game details
         find_img = soup.find("img",{"class":"game_header_image_full"})
@@ -47,11 +51,40 @@ for i,j in zip(find_links, find_final_price):
         img_url = find_img.get("src")
 
         price_fixed = j.text.replace("Rp","").replace(" ","")
-        print(f"img = {img_url}\ntitle = {find_title.text}\nharga = {price_fixed}\ndeskripsi = {find_description.text.strip()}\ndate = {find_realese_date.text}\n============")
 
-        with open(f"image\\{find_title.text.replace(" ","_").replace("™","")}.jpg","wb") as file:
+        data = {
+            "id": n,
+            "title": find_title.text,
+            "price": price_fixed,
+            "short_desk": find_description.text.strip(),
+            "date": find_realese_date.text,
+            "img_src_name": get_title_from_url
+        }
+
+        #saving image
+        with open(f"image\\{get_title_from_url}.jpg","wb") as file:
             response_img = requests.get(img_url)
             file.write(response_img.content)
+
+        #read json file is blank or no
+        try:
+            with open(json_name,"r",encoding="utf-8") as read_json:
+                existing_data = json.load(read_json)
+        except FileNotFoundError:
+            existing_data = []
+
+        #json dummy
+        existing_data.append(data)
+
+        #write json
+        with open(json_name,"w",encoding="utf-8") as write_json:
+            json.dump(existing_data,write_json,ensure_ascii=False,indent=4)
+
+        n += 1
+        if n == limit: #stop
+            break
+
+        print(f"Success {find_title.text}")
 
     except AttributeError:
         continue
